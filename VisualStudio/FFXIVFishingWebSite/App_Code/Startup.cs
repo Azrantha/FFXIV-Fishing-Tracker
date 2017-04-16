a36f96b9b198b11bf43248b91853d3daf9e993db
Azrantha <the_liger@hotmail.co.uk>
2017-04-16 20:08:28 +0100

    Creation

---
+++ VisualStudio/FFXIVFishingWebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FFXIVFishingWebSite.Startup))]
+namespace FFXIVFishingWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
